ee82922370f3150fd80c67fc3ac3c201ccb3cf2e
barkotenicholas <barkotenicholas@gmail.com>
2022-06-18 21:57:23 +0300

    Made necessary changes

--- app/src/main/java/com/barkote/travel/MainActivity.kt
+++ app/src/main/java/com/barkote/travel/MainActivity.kt
@@ -2,10 +2,21 @@ package com.barkote.travel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.barkote.travel.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController  : NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFrag = supportFragmentManager.findFragmentById(binding.navHostFrag.id) as NavHostFragment
+        navController = navHostFrag.navController
     }
 }
