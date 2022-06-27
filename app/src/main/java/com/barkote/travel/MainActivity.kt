b87c9951a54f41036c4db2c657bb9cfa61e90a95
Nicholas Barkote <barkotenicholas@gmail.com>
2022-06-27 21:18:43 +0300

    Added onBackpress

--- app/src/main/java/com/barkote/travel/MainActivity.kt
+++ app/src/main/java/com/barkote/travel/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.barkote.travel.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -18,5 +20,20 @@ class MainActivity : AppCompatActivity() {
 
         val navHostFrag = supportFragmentManager.findFragmentById(binding.navHostFrag.id) as NavHostFragment
         navController = navHostFrag.navController
+
+        val appBarConfig = AppBarConfiguration(navController.graph,binding.drawer)
+
+        binding.activityMainToolbar.setupWithNavController(navController,appBarConfig)
+
+        binding.navView.setupWithNavController(navController)
+
+    }
+
+    override fun onBackPressed() {
+        if(binding.drawer.isOpen){
+            binding.drawer.close()
+        }else{
+            super.onBackPressed()
+        }
     }
 }
