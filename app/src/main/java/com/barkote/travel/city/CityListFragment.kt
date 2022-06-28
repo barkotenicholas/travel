1c27195bce4f3bdb6699389f7a4e5c43528e5bad
Nicholas Barkote <barkotenicholas@gmail.com>
2022-06-28 14:01:49 +0300

    Added swipe to delete

--- app/src/main/java/com/barkote/travel/city/CityListFragment.kt
+++ app/src/main/java/com/barkote/travel/city/CityListFragment.kt
@@ -5,11 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.barkote.travel.databinding.FragmentCityListBinding
-import java.util.*
 
 
 class CityListFragment : Fragment() {
--- app/src/main/java/com/barkote/travel/favorite/FavoriteFragment.kt
+++ app/src/main/java/com/barkote/travel/favorite/FavoriteFragment.kt
@@ -1,4 +1,5 @@
 package com.barkote.travel.favorite
+
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,11 +8,10 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.barkote.travel.R
 import com.barkote.travel.city.City
 import com.barkote.travel.city.VacationSpots
-import com.barkote.travel.databinding.FragmentCityListBinding
 import com.barkote.travel.databinding.FragmentFavoriteBinding
+import com.google.android.material.snackbar.Snackbar
 import com.sriyank.globotour.favorite.FavoriteAdapter
 import java.util.*
 import kotlin.collections.ArrayList
@@ -20,9 +20,13 @@ import kotlin.collections.ArrayList
 class FavoriteFragment : Fragment() {
 
     private lateinit var binding: FragmentFavoriteBinding
-    private lateinit var favoriteCityList : ArrayList<City>
-    private lateinit var favoriteAdapter : FavoriteAdapter
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+    private lateinit var favoriteCityList: ArrayList<City>
+    private lateinit var favoriteAdapter: FavoriteAdapter
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
 
         binding = FragmentFavoriteBinding.inflate(inflater, container, false)
 
@@ -34,7 +38,7 @@ class FavoriteFragment : Fragment() {
         val context = requireContext()
 
         favoriteCityList = VacationSpots.favoriteCityList as ArrayList<City>
-        favoriteAdapter  = FavoriteAdapter(context, favoriteCityList)
+        favoriteAdapter = FavoriteAdapter(context, favoriteCityList)
 
         binding.favrecylerview.adapter = favoriteAdapter
         binding.favrecylerview.setHasFixedSize(true)
@@ -47,24 +51,57 @@ class FavoriteFragment : Fragment() {
     }
 
     private val itemTouchHelper = ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(
-        ItemTouchHelper.UP or ItemTouchHelper.DOWN,0){
+        ItemTouchHelper.UP or ItemTouchHelper.DOWN, ItemTouchHelper.RIGHT
+    ) {
         override fun onMove(
             recyclerView: RecyclerView,
             viewHolder: RecyclerView.ViewHolder,
             target: RecyclerView.ViewHolder
         ): Boolean {
             val from = viewHolder.adapterPosition
-            val to   = target.adapterPosition
+            val to = target.adapterPosition
 
-            Collections.swap(favoriteCityList,from,to)
+            Collections.swap(favoriteCityList, from, to)
 
-            favoriteAdapter.notifyItemMoved(from,to)
+            favoriteAdapter.notifyItemMoved(from, to)
             return true
         }
 
         override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-            TODO("Not yet implemented")
+
+            val position = viewHolder.adapterPosition
+            val city : City = favoriteCityList[viewHolder.adapterPosition]
+
+            deleteAtPosition(position)
+            updateCityList(city,false)
+
+            Snackbar.make(binding.favrecylerview, "Deleted", Snackbar.LENGTH_LONG).setAction("Undo"
+            ) {
+                undoDelete(city, position)
+                updateCityList(city,true)
+
+            }.show()
+
         }
 
     })
+
+    private fun undoDelete(city: City, position: Int) {
+        favoriteCityList.add(city)
+        favoriteAdapter.notifyItemInserted(position)
+        favoriteAdapter.notifyItemRangeChanged(position,favoriteCityList.size)
+    }
+
+    private fun updateCityList(city: City,isFav: Boolean) {
+        var cityList = VacationSpots.cityList!!
+        val position = cityList.indexOf(city)
+        cityList[position].isFavourite = isFav
+    }
+
+    private fun deleteAtPosition(adapterPosition: Int) {
+
+        favoriteCityList.removeAt(adapterPosition)
+        favoriteAdapter.notifyItemRemoved(adapterPosition)
+        favoriteAdapter.notifyItemRangeChanged(adapterPosition, favoriteCityList.size)
+    }
 }
